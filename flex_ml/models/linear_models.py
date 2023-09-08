0606f4ed3d35178d9174c3ef3562f619b92e7586
cbcote <christopher.cote88@gmail.com>
2023-09-07 23:47:00 -0500

    removed __init__ docstring and fixed double quotes

--- flex_ml/models/linear_models.py
+++ flex_ml/models/linear_models.py
@@ -5,29 +5,8 @@ import matplotlib.pyplot as plt
 
 
 class LinearRegression(BaseModel):
-    def __init__(self, method='ols', fit_intercept=True, normalize=False, 
+    def __init__(self, method="ols", fit_intercept=True, normalize=False, 
                  learning_rate=0.01, epochs=1000, regularization=None, alpha=0.1) -> None:
-        super().__init__()
-        """
-        Parameters
-        ----------
-        method : str, optional
-            Method to used for fitting the model. The default is 'ols'.
-            Can be either 'ols' or 'gradient_descent'.
-        fit_intercept : bool, optional
-            Whether to fit an intercept term. The default is True.
-        normalize : bool, optional
-            Whether to normalize the feature matrix. The default is False.
-        learning_rate : float, optional
-            Learning rate for gradient descent (hyperparameter). The default is 0.01.
-        epochs : int, optional
-            Number of epochs for gradient descent (hyperparameter). The default is 1000.
-        regularization : str, optional
-            Regularization method to use; can be 'l1' (Lasso) or 'l2' (Ridge), or None (No regularzation).
-            The default is None.
-        alpha : float, optional
-            Regularization strength. The default is 0.1.
-        """
         self.method = method
         self.fit_intercept = fit_intercept
         self.normalize = normalize
@@ -55,9 +34,9 @@ class LinearRegression(BaseModel):
         if self.fit_intercept:
             X = np.hstack((np.ones((X.shape[0], 1)), X))
         
-        if self.method == 'ols':
+        if self.method == "ols":
             self._fit_ols(X, y)
-        elif self.method == 'gradient_descent':
+        elif self.method == "gradient_descent":
             self._fit_gradient_descent(X, y)
         else:
             raise ValueError("Invalid method specified. Choose either 'ols' or 'gradient_descent'")
@@ -74,9 +53,9 @@ class LinearRegression(BaseModel):
             Target vector
         """
         XtX = np.dot(X.T, X)
-        if self.regularization == 'l1':
+        if self.regularization == "l1":
             XtX += self.alpha * np.eye(XtX.shape[1])
-        elif self.regularization == 'l2':
+        elif self.regularization == "l2":
             XtX += self.alpha * np.eye(XtX.shape[1])
         
         XtX_inv = np.linalg.inv(XtX)
@@ -108,12 +87,12 @@ class LinearRegression(BaseModel):
             errors = y - predictions
             gradient = 2 * np.dot(X.T, errors) / X.shape[0]
         
-        if self.regularization == 'l1':
-            gradient += self.alpha * np.sign(coef)
-        elif self.regularization == 'l2':
-            gradient += self.alpha * coef
-        
-        coef -= self.learning_rate * gradient
+            if self.regularization == "l1":
+                gradient += self.alpha * np.sign(coef)
+            elif self.regularization == "l2":
+                gradient += self.alpha * coef
+            
+            coef -= self.learning_rate * gradient
         
         if self.fit_intercept:
             self.intercept_ = coef[0]
@@ -151,8 +130,8 @@ class LinearRegression(BaseModel):
     def get_params(self):
         """Return model parameters as a dictionary"""
         return {
-            'fit_intercept': self.fit_intercept,
-            'normalize': self.normalize
+            "fit_intercept": self.fit_intercept,
+            "normalize": self.normalize
         }
     
     def set_params(self, **params):
@@ -202,7 +181,7 @@ class LinearRegression(BaseModel):
         filename : str
             Name of file to save model to
         """
-        with open(filename, 'wb') as f:
+        with open(filename, "wb") as f:
             pickle.dump(self, f)
     
     @classmethod
@@ -214,7 +193,7 @@ class LinearRegression(BaseModel):
         filename : str
             Name of file to load model from
         """
-        with open(filename, 'rb') as f:
+        with open(filename, "rb") as f:
             return pickle.load(f)
     
     def plot_residuals(self, X, y):
@@ -230,6 +209,6 @@ class LinearRegression(BaseModel):
         y_pred = self.predict(X)
         residuals = y - y_pred
         plt.scatter(y_pred, residuals)
-        plt.xlabel('Predicted')
-        plt.ylabel('Residuals')
+        plt.xlabel("Predicted")
+        plt.ylabel("Residuals")
         plt.show()
